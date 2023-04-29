802fc9d19bac319720d79d21af9b60d651d75011
kmnviz <kamenovivanzdravkov@gmail.com>
2023-04-29 13:11:47 +0300

    rework user page

--- src/pages/[user]/index.js
+++ src/pages/[user]/index.js
@@ -12,8 +12,8 @@ export default function User({username, profile, items, user}) {
     const [selectedAudioState, setSelectedAudioState] = useState(false);
     const [selectedAudioDuration, setSelectedAudioDuration] = useState(0);
     const [audioElementCurrentTime, setAudioElementCurrentTime] = useState(0);
-    const [avatarImage, setAvatarImage] = useState(profile && profile?.avatar ? `${process.env.IMAGES_URL}/1920_${profile.avatar}` : '');
-    const [backgroundImage, setBackgroundImage] = useState(profile && profile?.background ? `${process.env.IMAGES_URL}/1920_${profile.background}` : '');
+    const [avatarImage] = useState(profile && profile?.avatar ? `${process.env.IMAGES_URL}/1920_${profile.avatar}` : '');
+    const [backgroundImage] = useState(profile && profile?.background ? `${process.env.IMAGES_URL}/1920_${profile.background}` : '');
 
     const handleAudioChange = (src) => {
         const audioElement = document.getElementById('audio');
@@ -65,98 +65,87 @@ export default function User({username, profile, items, user}) {
                 <audio id="audio" type="audio/mpeg" className="invisible"/>
                 <div className="w-full flex justify-center">
                     <div className="w-full max-w-screen-2xl">
-                        <div className="w-full h-24 lg:h-16 py-4 lg:py-0 px-8 flex flex-col lg:flex-row justify-between items-center">
+                        <div
+                            className="w-full h-16 py-4 lg:py-0 px-8 flex flex-row justify-between items-center">
                             <h6 className="font-grotesk text-xl select-none">{username}</h6>
                             <div className="h-full flex items-center">
                                 <div
-                                    className="w-10 h-10 flex justify-center items-center relative hover:cursor-pointer">
+                                    className="w-10 h-10 flex justify-center items-center relative hover:cursor-pointer group">
                                     <div className="w-full h-full absolute flex justify-center items-center z-10"></div>
-                                    <div className="w-full h-full absolute flex justify-center items-center z-0">
-                                        <img src="/instagram.svg" className="w-6 h-6 rounded-full invert"/>
-                                    </div>
-                                </div>
-                                <div
-                                    className="w-10 h-10 flex justify-center items-center relative hover:cursor-pointer">
-                                    <div className="w-full h-full absolute flex justify-center items-center z-10"></div>
-                                    <div className="w-full h-full absolute flex justify-center items-center z-0">
-                                        <img src="/facebook.svg" className="w-6 h-6 rounded-full invert"/>
-                                    </div>
-                                </div>
-                                {
-                                    (user && user.username === username) &&
                                     <div
-                                        className="w-10 h-10 flex justify-center items-center relative hover:cursor-pointer"
-                                        onClick={() => router.push('/dashboard')}>
-                                        <div
-                                            className="w-full h-full absolute flex justify-center items-center z-10"></div>
-                                        <div className="w-full h-full absolute flex justify-center items-center z-0">
-                                            <img src="/user.svg" className="w-6 h-6 rounded-full"/>
-                                        </div>
+                                        className="w-full h-full absolute flex justify-center items-center z-0 group-hover:rounded-md group-hover:shadow">
+                                        <img src="/share.svg" className="w-6 h-6"/>
                                     </div>
-                                }
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                {
-                    profile &&
-                    <>
-                        <div className="w-full h-auto lg:h-80 relative flex justify-center">
-                            <div className="w-full h-full absolute bg-cover bg-center blur-sm grayscale z-0"
-                                 style={{backgroundImage: `url(${backgroundImage})`}}></div>
-                            <div className="w-full h-full max-w-screen-2xl px-8 z-10 flex flex-col lg:flex-row items-center z-10">
-                                <div className="w-40 lg:w-64 h-40 lg:h-64 mt-8 lg:mt-0 bg-cover bg-center rounded-lg"
+                <div className="w-full h-64 md:h-80 relative">
+                    <div className="w-full h-56 md:h-72 absolute top-0 left-0 z-10 bg-cover bg-center"
+                         style={{backgroundImage: `url(${backgroundImage})`}}></div>
+                    <div className="w-full h-40 absolute bottom-0 left-0 z-20 flex justify-center">
+                        <div className="w-full max-w-screen-2xl">
+                            <div className="w-full px-8">
+                                <div className="w-40 h-40 rounded-full border-white border-4 shadow bg-cover bg-center"
                                      style={{backgroundImage: `url(${avatarImage})`}}></div>
-                                <div className="hidden lg:w-16 lg:block"></div>
-                                <div className="block h-8 lg:hidden"></div>
-                                <div className="h-auto lg:h-64">
-                                    <div className="h-full flex items-center">
-                                        <h1 className="font-grotesk font-bold text-white text-center lg:text-right text-5xl lg:text-7xl">{profile && profile?.name ? profile.name : ''}</h1>
-                                    </div>
-                                </div>
                             </div>
                         </div>
-                    </>
-                }
-                <div className="h-32"></div>
-                <div className="w-full flex justify-center">
+                    </div>
+                </div>
+                <div className="w-full flex justify-center mt-4">
                     <div className="w-full max-w-screen-2xl px-8">
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+                        <h2 className="font-grotesk text-3xl">{profile.name}</h2>
+                        <p className="font-grotesk mt-8">Items: {items.length}</p>
+                        <div className="w-full h-px mt-1 bg-gray-300"></div>
+                        <div
+                            className="w-full mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
                             {
                                 items.map((item, index) => {
                                     return (
-                                        <div className="h-96 rounded-lg relative shadow-md"
-                                             style={{backgroundImage: `url(${process.env.IMAGES_URL}/480_${item.image})`}}
-                                             key={`item-${index}`}
+                                        <div key={`item-${item._id}`}
+                                             className="relative flex flex-col rounded-md shadow hover:shadow-lg cursor-pointer group"
                                         >
-                                            <div
-                                                className="w-full h-full rounded-lg absolute black-to-transparent-gradient"></div>
-                                            {
-                                                item?.audio && item?.audio_preview &&
-                                                <div className="w-full h-full absolute flex justify-center items-center"
-                                                     onClick={() => handleAudioChange(item.audio_preview)}>
-                                                    <div className="w-16 h-16 -mt-16 hover:cursor-pointer">
+                                            <div className="w-full h-64 relative">
+                                                <div className="w-full h-full absolute top-0 left-0 bg-cover bg-center rounded-t-md z-10"
+                                                     style={{backgroundImage: `url(${process.env.IMAGES_URL}/480_${item.image})`}}
+                                                ></div>
+                                                <div className="w-full h-full absolute top-0 left-0 rounded-t-md z-20">
+                                                    <div className="w-full h-full relative">
+                                                        <div className="w-full h-full absolute top-0 left-0 bg-black
+                                                            rounded-t-md opacity-0 group-hover:opacity-25"
+                                                        ></div>
                                                         {
-                                                            selectedAudioState && selectedAudio === item.audio_preview
-                                                                ?
-                                                                <img src="/pause.svg" className="w-full h-full invert"/>
-                                                                :
-                                                                <img src="/play.svg" className="w-full h-full invert"/>
+                                                            item?.audio_preview &&
+                                                            <div className="w-full h-full absolute top-0 left-0 flex justify-center items-center">
+                                                                <div className={`w-12 h-12 group-hover:block bg-white rounded shadow 
+                                                                ${!(selectedAudio === item.audio_preview) ? 'hidden' : '' }`}
+                                                                     onClick={() => handleAudioChange(item.audio_preview)}
+                                                                >
+                                                                    {
+                                                                        selectedAudioState && selectedAudio === item.audio_preview
+                                                                            ?
+                                                                            <img src="/pause.svg" className="w-full h-full"/>
+                                                                            :
+                                                                            <img src="/play.svg" className="w-full h-full"/>
+                                                                    }
+                                                                </div>
+                                                            </div>
                                                         }
                                                     </div>
                                                 </div>
-                                            }
-                                            <div className="w-full absolute bottom-0 p-4 flex flex-col">
-                                                <p className="font-grotesk text-base text-white h-12 overflow-hidden overflow-ellipsis">{item.name}</p>
-                                                <div className="h-6"></div>
-                                                <div
-                                                    className="w-full h-10 px-4 bg-black border rounded-lg flex justify-between items-center hover:cursor-pointer group"
-                                                    onClick={() => router.push(`/checkout?itemId=${item._id}&username=${username}`)}>
+                                            </div>
+                                            <div className="w-full p-4">
+                                                <p className="font-grotesk truncate">{item.name}{item.name}{item.name}</p>
+                                                <div className="flex justify-end items-center mt-4">
                                                     <div
-                                                        className="h-full flex items-center group-hover:pl-2 duration-200">
-                                                        <img src="/cart.svg" className="w-6 h-6 invert"/>
+                                                        className="px-2 py-0.5 rounded shadow flex justify-between items-center
+                                                        bg-blue-100 hover:shadow-lg hover:bg-blue-300"
+                                                        onClick={() => router.push(`/checkout?itemId=${item._id}&username=${username}`)}
+                                                    >
+                                                        <img src="/cart.svg" className="w-4 h-4 mr-4"/>
+                                                        ${formatAmount(item.price)}
                                                     </div>
-                                                    <p className="font-grotesk text-base text-white">${formatAmount(item.price)}</p>
                                                 </div>
                                             </div>
                                         </div>
